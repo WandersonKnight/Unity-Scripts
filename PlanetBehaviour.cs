be3fe75d10181b22c256fe4f6da2a8315e47c19d
Wanderson Knight <39245594+WandersonKnight@users.noreply.github.com>
2021-02-13 15:38:10 -0300

    Update PlanetBehaviour.cs

--- PlanetBehaviour.cs
+++ PlanetBehaviour.cs
@@ -1,22 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-//This script gives an auto one direction rotate planet like behaviour to the 3D sphere that it's atached. 
-//It also makes it draggable and adds inertia to it if it's spinned with mouse/touch.
+using UnityEngine.UI;
 
 public class PlanetBehaviour : MonoBehaviour
 {
-    //Drag speed
-    float rotSpeed = 700f;
 
-    //Inertia speed
-    public float amount = 20f;
+    Touch touch;
+
+    float rotSpeedPc = 700f;
+    float rotSpeedMobile = 10f;
+    public float amountPC = 6f;
+    public float amountMobile = 8f;
 
     void FixedUpdate()
     {
 
-        //When playing on Windows
         if (Application.platform == RuntimePlatform.WindowsEditor)
         {
 
@@ -27,36 +26,13 @@ public class PlanetBehaviour : MonoBehaviour
 
             }
             else
-
-            //Maintain Minimun autonomous speed
             {
 
                 gameObject.GetComponent<Rigidbody>().angularDrag = 0.4f;
 
             }
 
-            if (Input.GetMouseButtonUp(0))
-            {
-                float x = -Input.GetAxis("Mouse X") * amount * Time.fixedDeltaTime;
-                gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * x, ForceMode.VelocityChange);
-
-            }
-
-            else if (Input.GetMouseButton(0))
-            {
-
-                transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0) * Time.fixedDeltaTime * rotSpeed);
-
-            }
-
-            if (Input.GetMouseButtonDown(0))
-            {
-
-                gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-
-            }
-            //Minimun autonomous speed
-            if (!Input.GetMouseButtonDown(0))
+            if (!Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0))
             {
 
                 transform.Rotate(new Vector3(0, -0.4f, 0));
@@ -65,11 +41,8 @@ public class PlanetBehaviour : MonoBehaviour
 
         }
 
-        //When playing on Android
         if (Application.platform == RuntimePlatform.Android)
         {
-
-            Touch touch = Input.GetTouch(0);
 
             if (gameObject.GetComponent<Rigidbody>().angularVelocity.magnitude < 0.25f)
             {
@@ -77,8 +50,6 @@ public class PlanetBehaviour : MonoBehaviour
                 gameObject.GetComponent<Rigidbody>().angularDrag = 0;
 
             }
-
-            //Maintain Minimun autonomous speed
             else
             {
 
@@ -86,34 +57,98 @@ public class PlanetBehaviour : MonoBehaviour
 
             }
 
-            if (touch.phase == TouchPhase.Ended)
+            if (Input.touchCount == 0)
             {
 
-                float pointer_x = touch.deltaPosition.x;
-                float x = pointer_x * amount * Time.fixedDeltaTime;
-                gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * -x, ForceMode.VelocityChange);
+                transform.Rotate(new Vector3(0, -0.4f, 0));
 
             }
 
-            if (touch.phase == TouchPhase.Moved)
+        }
+
+    }
+
+    void OnMouseDrag()
+    {
+
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+
+            if (!Input.GetMouseButtonDown(0) && !Input.GetMouseButtonUp(0))
             {
 
-                float pointer_x = touch.deltaPosition.x;
-                transform.Rotate(new Vector3(0, -pointer_x, 0));
+                transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0) * Time.fixedDeltaTime * rotSpeedPc);
 
             }
 
-            if (touch.phase == TouchPhase.Began)
+        }
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.touchCount > 0)
+            {
+
+                touch = Input.GetTouch(0);
+
+                if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Began)
+                {
+
+                    float pointer_x = touch.deltaPosition.x;
+                    transform.Rotate(new Vector3(0, -pointer_x, 0) * Time.fixedDeltaTime * rotSpeedMobile);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    void LateUpdate()
+    {
+
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+
+            if (Input.GetMouseButtonDown(0))
             {
 
                 gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
 
             }
-            //Minimun autonomous speed
-            if (Input.touchCount == 0)
+
+            if (Input.GetMouseButtonUp(0) && !Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0))
+            {
+                float x = -Input.GetAxis("Mouse X") * amountPC * Time.fixedDeltaTime;
+                gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * x, ForceMode.VelocityChange);
+
+            }
+
+        }
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            touch = Input.GetTouch(0);
+
+            if (Input.touchCount > 0)
             {
 
-                transform.Rotate(new Vector3(0, -0.4f, 0));
+                if(touch.phase == TouchPhase.Began)
+                {
+
+                    gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+                }
+
+                if (touch.phase == TouchPhase.Ended && touch.phase != TouchPhase.Moved && touch.phase != TouchPhase.Began)
+                {
+
+                    float pointer_x = touch.deltaPosition.x;
+                    float x = -pointer_x * amountMobile * Time.fixedDeltaTime;
+                    gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * x, ForceMode.VelocityChange);
+
+                }
 
             }
 
