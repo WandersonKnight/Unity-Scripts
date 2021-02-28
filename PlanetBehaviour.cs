a5bac156e437c0e85a3380161d9314510f317bd6
Wanderson Knight <39245594+WandersonKnight@users.noreply.github.com>
2021-02-28 07:05:59 -0300

    Update PlanetBehaviour.cs

--- PlanetBehaviour.cs
+++ PlanetBehaviour.cs
@@ -1,66 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlanetBehaviour : MonoBehaviour
 {
 
     Touch touch;
 
-    float rotSpeedPc = 700f;
+    float rotSpeedPc = 700f; // Rotation speed of the drag
     float rotSpeedMobile = 10f;
-    public float amountPC = 6f;
-    public float amountMobile = 8f;
+    float amountPC = 25f; // Inertia speed
+    float amountMobile = 8f;
+    int locked = 0; // Change is not advisible
+
 
     void FixedUpdate()
     {
 
-        if (Application.platform == RuntimePlatform.WindowsEditor)
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Application.platform == RuntimePlatform.WindowsEditor) // Code to work on windows
         {
+            
+            gameObject.GetComponent<Rigidbody>().angularDrag = 0.4f;
 
-            if (gameObject.GetComponent<Rigidbody>().angularVelocity.magnitude < 0.25f)
-            {
-
-                gameObject.GetComponent<Rigidbody>().angularDrag = 0;
-
-            }
-            else
-            {
-
-                gameObject.GetComponent<Rigidbody>().angularDrag = 0.4f;
-
-            }
-
-            if (!Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0))
+            if (!Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0) || !Physics.Raycast(ray) && locked == 0) // Natural rotation
             {
 
                 transform.Rotate(new Vector3(0, -0.4f, 0));
 
+            }
+
+            if (Input.GetMouseButtonDown(0)) // Stop rotation when clicked
+            {
+
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                Physics.Raycast(ray, out hit);
+
+                if(hit.collider != null)
+                {
+
+                    if (hit.collider.CompareTag("Planet")) // Tag on the planet object
+                    {
+
+                        gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                        locked = 1;
+
+                    }
+
+                }
+          
             }
 
         }
 
-        if (Application.platform == RuntimePlatform.Android)
+        if (Application.platform == RuntimePlatform.Android) // Code to work on mobile
         {
 
-            if (gameObject.GetComponent<Rigidbody>().angularVelocity.magnitude < 0.25f)
-            {
+            gameObject.GetComponent<Rigidbody>().angularDrag = 0.4f;
 
-                gameObject.GetComponent<Rigidbody>().angularDrag = 0;
-
-            }
-            else
-            {
-
-                gameObject.GetComponent<Rigidbody>().angularDrag = 0.4f;
-
-            }
-
-            if (Input.touchCount == 0)
+            if (Input.touchCount == 0 || !Physics.Raycast(ray) && locked == 0) // Natural Rotation
             {
 
                 transform.Rotate(new Vector3(0, -0.4f, 0));
+
+            }
+
+            if(Input.touchCount > 0) // Stop rotation when clicked
+            {
+
+                if (touch.phase == TouchPhase.Began)
+                {
+
+                    ray = Camera.main.ScreenPointToRay(touch.position);
+
+                    Physics.Raycast(ray, out hit);
+                    if (hit.collider != null)
+                    {
+
+                        if (hit.collider.CompareTag("Planet")) // Tag on the planet object
+                        {
+
+                            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                            locked = 1;
+
+                        }
+
+                    }
+
+                }
 
             }
 
@@ -68,10 +99,10 @@ public class PlanetBehaviour : MonoBehaviour
 
     }
 
-    void OnMouseDrag()
+    void OnMouseDrag() // Movement when dragged 
     {
 
-        if (Application.platform == RuntimePlatform.WindowsEditor)
+        if (Application.platform == RuntimePlatform.WindowsEditor) // On Windows
         {
 
             if (!Input.GetMouseButtonDown(0) && !Input.GetMouseButtonUp(0))
@@ -83,14 +114,14 @@ public class PlanetBehaviour : MonoBehaviour
 
         }
 
-        if (Application.platform == RuntimePlatform.Android)
+        if (Application.platform == RuntimePlatform.Android) // On mobile
         {
             if (Input.touchCount > 0)
             {
 
                 touch = Input.GetTouch(0);
 
-                if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Began)
+                if (touch.phase != TouchPhase.Began && touch.phase != TouchPhase.Ended)
                 {
 
                     float pointer_x = touch.deltaPosition.x;
@@ -104,21 +135,17 @@ public class PlanetBehaviour : MonoBehaviour
 
     }
 
+
     void LateUpdate()
     {
 
-        if (Application.platform == RuntimePlatform.WindowsEditor)
+        if (Application.platform == RuntimePlatform.WindowsEditor) // On Windows
         {
 
-            if (Input.GetMouseButtonDown(0))
+            if (locked == 1 && Input.GetMouseButtonUp(0) && !Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0)) // Inertia after released
             {
 
-                gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-
-            }
-
-            if (Input.GetMouseButtonUp(0) && !Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0))
-            {
+                locked = 0;
                 float x = -Input.GetAxis("Mouse X") * amountPC * Time.fixedDeltaTime;
                 gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * x, ForceMode.VelocityChange);
 
@@ -126,24 +153,18 @@ public class PlanetBehaviour : MonoBehaviour
 
         }
 
-        if (Application.platform == RuntimePlatform.Android)
+        if (Application.platform == RuntimePlatform.Android) // On mobile
         {
-
-            touch = Input.GetTouch(0);
 
             if (Input.touchCount > 0)
             {
 
-                if(touch.phase == TouchPhase.Began)
+                touch = Input.GetTouch(0);
+
+                if (locked == 1 && touch.phase == TouchPhase.Ended && touch.phase != TouchPhase.Moved && touch.phase != TouchPhase.Began) // Inertia after released
                 {
 
-                    gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-
-                }
-
-                if (touch.phase == TouchPhase.Ended && touch.phase != TouchPhase.Moved && touch.phase != TouchPhase.Began)
-                {
-
+                    locked = 0;
                     float pointer_x = touch.deltaPosition.x;
                     float x = -pointer_x * amountMobile * Time.fixedDeltaTime;
                     gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * x, ForceMode.VelocityChange);
